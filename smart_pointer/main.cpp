68e3a3bfa1935fdaa39d6f3de1974e11534dbc3e
rukudias <rukudias@gmail.com>
2021-04-10 19:15:39 +0300

    some changes

--- smart_pointer/main.cpp
+++ smart_pointer/main.cpp
@@ -2,10 +2,53 @@
 #include <memory> // для std::shared_ptr
 #include <string>
 //cyclical references
+//std::make_shared/unique избегать исключений/ошибок в конструкторах
+//class Human
+//{
+//    std::string m_name;
+//    std::shared_ptr<Human> m_partner; // изначально пустой
+//
+//public:
+//
+//    Human(const std::string &name): m_name(name)
+//    {
+//        std::cout << m_name << " created\n";
+//    }
+//    ~Human()
+//    {
+//        std::cout << m_name << " destroyed\n";
+//    }
+//
+//    friend bool partnerUp(std::shared_ptr<Human> &anton, std::shared_ptr<Human> &ivan)
+//    {
+//        if (!anton || !ivan)
+//            return false;
+//
+//        anton->m_partner = ivan;
+//        ivan->m_partner = anton;
+//
+//        std::cout << anton->m_name << " is now partnered with " << ivan->m_name << "\n";
+//
+//        return true;
+//    }
+//};
+//
+//int main()
+//{
+//    auto anton = std::make_shared<Human>("Anton"); // создание умного указателя с объектом Anton класса Human
+//    auto ivan = std::make_shared<Human>("Ivan"); // создание умного указателя с объектом Ivan класса Human
+//
+//    partnerUp(anton, ivan); // Anton указывает на Ivan-а, а Ivan указывает на Anton-а
+//
+//    return 0;
+//}
+//****************************************************************************
+//****************************************************************************
+//****************************************************************************
 class Human
 {
     std::string m_name;
-    std::shared_ptr<Human> m_partner; // изначально пустой
+    std::weak_ptr<Human> m_partner; // обратите внимание, здесь std::weak_ptr
 
 public:
 
@@ -18,15 +61,15 @@ public:
         std::cout << m_name << " destroyed\n";
     }
 
-    friend bool partnerUp(std::shared_ptr<Human> &anton, std::shared_ptr<Human> &ivan)
+    friend bool partnerUp(std::shared_ptr<Human> &h1, std::shared_ptr<Human> &h2)
     {
-        if (!anton || !ivan)
+        if (!h1 || !h2)
             return false;
 
-        anton->m_partner = ivan;
-        ivan->m_partner = anton;
+        h1->m_partner = h2;
+        h2->m_partner = h1;
 
-        std::cout << anton->m_name << " is now partnered with " << ivan->m_name << "\n";
+        std::cout << h1->m_name << " is now partnered with " << h2->m_name << "\n";
 
         return true;
     }
@@ -34,10 +77,10 @@ public:
 
 int main()
 {
-    auto anton = std::make_shared<Human>("Anton"); // создание умного указателя с объектом Anton класса Human
-    auto ivan = std::make_shared<Human>("Ivan"); // создание умного указателя с объектом Ivan класса Human
+    auto anton = std::make_shared<Human>("Anton");
+    auto ivan = std::make_shared<Human>("Ivan");
 
-    partnerUp(anton, ivan); // Anton указывает на Ivan-а, а Ivan указывает на Anton-а
+    partnerUp(anton, ivan);
 
     return 0;
 }
